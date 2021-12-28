37bf9369bb9eb43cfccdcade1a113bf701d3207c
2222-42 <stephen0617@me.com>
2021-12-29 08:58:55 +0900

    chore(*): iteratorについて動作確認

--- chap3/control_flow/src/main.rs
+++ chap3/control_flow/src/main.rs
@@ -1,7 +1,34 @@
+struct Iter {
+    current: usize,
+    max: usize,
+}
+
+impl Iterator for Iter {
+    type Item = usize; // 出力する型の紐付け
+
+    // impl next()
+    fn next(&mut self) -> Option<usize> {
+        self.current += 1;
+        if self.current - 1 < self.max {
+            Some(self.current - 1)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     let array = [0, 1, 2, 3, 4, 5];
     for element in &array {
         println!("element: {}", element)
+    }
+
+    let it = Iter {
+        current: 0,
+        max: 10,
+    };
+    for num in it {
+        println!("count: {}", num);
     }
 
     'main: loop {
