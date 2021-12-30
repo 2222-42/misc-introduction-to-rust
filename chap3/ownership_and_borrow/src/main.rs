c48098926e116a5e19bb3b30f8cc13d2c8813264
2222-42 <stephen0617@me.com>
2021-12-30 15:19:14 +0900

    chore(*): dropトレイトの動作確認
    保有しているリソース(メモリ、ファイル、ネットワーク接続など)があり、それの解放処理が必要な場合はこれを使う

--- chap3/ownership_and_borrow/src/main.rs
+++ chap3/ownership_and_borrow/src/main.rs
@@ -1,3 +1,11 @@
+struct Droppable;
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("Resource will be released!");
+    }
+}
+
 fn main() {
     let mut x = 5;
     let _y = &x;
@@ -5,4 +13,9 @@ fn main() {
     // dbg!(_y);
     dbg!(z);
     dbg!(x);
+
+    {
+        let d = Droppable;
+    }
+    println!("The Droppable should be released at the end of block.");
 }
