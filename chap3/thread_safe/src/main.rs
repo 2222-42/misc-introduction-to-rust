8a0becbcb81c96d29a149c3b25e44ffa681011cf
2222-42 <stephen0617@me.com>
2021-12-31 11:18:33 +0900

    chore(*): 共有メモリの動作確認

--- chap3/thread_safe/src/main.rs
+++ chap3/thread_safe/src/main.rs
@@ -1,17 +1,31 @@
-use std::thread;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn pass_closure() {
     let mut handles = Vec::new();
+    let data = Arc::new(Mutex::new(vec![1; 10])); // スレッド間で所有権を共有するためにRcを使う
+                                                  // -> マルチスレッドでの同時アクセスを可能にするためにRcではなく、Arcを使う
+                                                  // -> Arcでは書き換えを行えないので、別途、排他制御を行うためにMutexを使う
     for x in 0..10 {
+        let data_ref = data.clone(); // Rcの参照カウンタを増やす。
+                                     // →　Arcの参照カウントを増やす。Rcよりコストが余分にかかる。
+
         // クロージャがデフォルトでは参照をキャプチャするので、moveキーワードを使ってxの所有権をスレッドに移す
         handles.push(thread::spawn(move || {
-            println!("Hellow, world!: {}", x);
+            // lockを使いdataへの可変参照を得る
+            // lockは複数のスレッドが同時にアクセスしないことを保証するためのメカニズム。あるスレッドが取得したら他のスレッドは取得できない。
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
         }));
     }
 
     for handle in handles {
         let _ = handle.join();
     }
+
+    dbg!(data);
 }
 
 fn main() {
