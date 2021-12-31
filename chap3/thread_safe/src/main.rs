af923d862407f22bdaf73e7734bcb79f85090a0e
2222-42 <stephen0617@me.com>
2021-12-31 09:20:55 +0900

    chore(*): moveを使ってみる

--- chap3/thread_safe/src/main.rs
+++ chap3/thread_safe/src/main.rs
@@ -1,5 +1,19 @@
 use std::thread;
 
+fn pass_closure() {
+    let mut handles = Vec::new();
+    for x in 0..10 {
+        // クロージャがデフォルトでは参照をキャプチャするので、moveキーワードを使ってxの所有権をスレッドに移す
+        handles.push(thread::spawn(move || {
+            println!("Hellow, world!: {}", x);
+        }));
+    }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+}
+
 fn main() {
     // スレッドのハンドル
     let handle = thread::spawn(|| // クロージャを受け取り、それを新しいスレッドで実行する
@@ -8,4 +22,6 @@ fn main() {
     });
 
     let _wait = dbg!(handle.join()); // joinを読んで、スレッドの終了を待つ。
+
+    pass_closure();
 }
