d0df3baad56edf9e721a1c5f1792493c4433a69b
2222-42 <stephen0617@me.com>
2022-01-25 12:22:09 +0900

    chore(chap11): 手続きマクロの関数的マクロの練習

--- chap11/sample_macro/src/main.rs
+++ chap11/sample_macro/src/main.rs
@@ -1,3 +1,5 @@
+use tomlstruct::tomlstruct;
+
 // Rustのマクロ置換は構文解析後の構文木に対しておこな合われるので、展開前に評価された演算子の優先順位が展開後も維持される。
 macro_rules! five_times {
     ($x:expr) => {
@@ -25,9 +27,20 @@ macro_rules! vec_multi_item {
     }};
 }
 
+tomlstruct! {
+    [Hello]
+    name = "hello"
+    version = 1.0
+}
+
 fn main() {
     assert_eq!(25, five_times!(2 + 3));
     println!("vec_one_item!: {:?}", vec_one_item![1]);
     println!("vec_multi_item!: {:?}", vec_multi_item![1, 2, 3]);
     println!("vec_multi_item!: {:?}", vec_multi_item![i32]);
+
+    let _ = Hello {
+        name: String::from("hello"),
+        version: 1.0,
+    };
 }
