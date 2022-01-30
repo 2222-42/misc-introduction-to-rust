9eeba10c1023414671d3f808cedc474a46e52414
2222-42 <stephen0617@me.com>
2022-01-30 13:35:43 +0900

    chore(chap11): resume_unwindとDropの使い方

--- chap11/sample_panic/src/main.rs
+++ chap11/sample_panic/src/main.rs
@@ -1,9 +1,19 @@
-use std::panic::catch_unwind;
+use std::panic::{catch_unwind, resume_unwind};
 
 fn print_range(x: &[i32]) {
     let min = x.iter().min().unwrap();
     let max = x.iter().max().unwrap();
     eprintln!("max - min = {}", max - min);
+}
+
+struct A(bool);
+impl Drop for A {
+    // Drop処理内ではパニックが起きそうな処理を避けること(回復処理が行われなくなる)
+    fn drop(&mut self) {
+        if self.0 {
+            eprintln!("A::drop");
+        }
+    }
 }
 
 fn main() {
@@ -39,4 +49,22 @@ fn main() {
         assert!(true);
     });
     eprintln!("is_ok = {}", thread2.join().is_ok());
+
+    let mut flag = A(true);
+    assert!(false);
+    flag.0 = false;
+    eprintln!("Success!");
+
+    let result = catch_unwind(|| {
+        assert!(false);
+    });
+    if let Err(payload) = result {
+        eprintln!("assert!(false) failed");
+        // パニック処理を続行する　→ eprintln!("success");は実行されない
+        resume_unwind(payload); // resume_unwindが役立つケースのひとつはFFI教会を超える必要がる場合。
+    } else {
+        eprintln!("assert!(false) succeeded");
+    }
+
+    eprintln!("success");
 }
